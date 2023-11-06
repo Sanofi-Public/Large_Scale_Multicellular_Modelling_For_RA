bb01799fe6b96d79e4ffd9b56a8513ad5ba208f6
naouel zerrouk <nawelzerrouk@gmail.com>
2023-11-06 13:52:17 +0000

    Update macro_fibro_SDY998_and_SDY998_datasets_processing.R

--- Intercellular interactions identification/From_sc_rna_seq/macro_fibro_SDY998_and_SDY998_datasets_processing.R
+++ Intercellular interactions identification/From_sc_rna_seq/macro_fibro_SDY998_and_SDY998_datasets_processing.R
@@ -3,15 +3,15 @@ library(plyr)
 library(sva)
 library(xlsx)
 
-setwd("C:/Users/I0471594/OneDrive - Sanofi/PhD/Macrophage_fibro_interactions/SANOFI_datasets")
+setwd("Large-scale multicellular modeling of the arthritic joint\\From_sc_rna_seq")
 
 
 #read fibroblast dataset
-metadata <- read.delim("BioTuring_SDY998_fibroblast_SC/SDY998_metadata.tsv")
+metadata <- read.delim("Large-scale multicellular modeling of the arthritic joint\\Datasets\\SDY998\\SDY998_metadata.tsv")
 
 #filter dataset
 metadata=metadata[(metadata$Cell.type=="Fibroblast" | metadata$Cell.type=="Monocyte")& metadata$Diagnosis=="Rheumatoid arthritis" ,1]
-seurat <- readRDS(file = "BioTuring_SDY998_fibroblast_SC/SDY998_seurat_1635258168512.Rds")
+seurat <- readRDS(file = "Large-scale multicellular modeling of the arthritic joint\\Datasets\\SDY998\\SDY998_seurat_1635258168512.Rds")
 exp_matrix=as.matrix(GetAssayData(object = seurat, slot = "counts"))
 exp_matrix_filtered=as.data.frame(exp_matrix[,metadata]) # normalized with log2(CPM)
 #remove genes with null variance
@@ -24,7 +24,7 @@ exp_matrix_filtered$ID=as.character(exp_matrix_filtered$ID)
 rownames(exp_matrix_filtered)=NULL
 
 
-metadata <- read.delim("BioTuring_SDY998_fibroblast_SC/SDY998_metadata.tsv")
+metadata <- read.delim("Large-scale multicellular modeling of the arthritic joint\\Datasets\\SDY998\\SDY998_metadata.tsv")
 metadata=metadata[(metadata$Cell.type=="Fibroblast" | metadata$Cell.type=="Monocyte")& metadata$Diagnosis=="Rheumatoid arthritis" ,c(1,7)]
 
 
