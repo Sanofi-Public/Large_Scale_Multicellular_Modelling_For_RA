d36a04b2dc6e7a4dae4a8ec571e9aa02104c839d
naouel zerrouk <nawelzerrouk@gmail.com>
2023-11-06 13:47:32 +0000

    Update macro_fibro_E_MTAB_8322_and_GSE109449_data_processing.R

--- Intercellular interactions identification/From_sc_rna_seq/macro_fibro_E_MTAB_8322_and_GSE109449_data_processing.R
+++ Intercellular interactions identification/From_sc_rna_seq/macro_fibro_E_MTAB_8322_and_GSE109449_data_processing.R
@@ -4,15 +4,15 @@ library(sva)
 library(xlsx)
 library(dplyr)
 
-setwd("C:/Users/I0471594/OneDrive - Sanofi/PhD/Macrophage_fibro_interactions/SANOFI_datasets")
+setwd("Large-scale multicellular modeling of the arthritic joint\\From_sc_rna_seq")
 
 #read macrophage dataset
-macro_metadata <- read.delim("Bioturing_E_MTAB_8322_macrophage_SC/STM/E_MTAB_8322_metadata.tsv")
+macro_metadata <- read.delim("Large-scale multicellular modeling of the arthritic joint\\Datasets\\E_MTAB_8322\\E_MTAB_8322_metadata.tsv")
 
 
 #keep only treatment naive RA
 macro_metadata=macro_metadata[macro_metadata$Group=="Naive rheumatoid arthritis",c(1)]
-exp_matrix=read.csv("Bioturing_E_MTAB_8322_macrophage_SC/STM/Merged.matrix.csv")
+exp_matrix=read.csv("Large-scale multicellular modeling of the arthritic joint\\Datasets\\E_MTAB_8322\\Merged.matrix.csv")
 exp_matrix_filtered_macro=exp_matrix[,macro_metadata]
 rownames(exp_matrix_filtered_macro)=exp_matrix[,1]
 #remove genes with var equal zero 
@@ -24,7 +24,7 @@ exp_matrix_filtered_macro$ID=as.character(exp_matrix_filtered_macro$ID)
 rownames(exp_matrix_filtered_macro)=NULL
 
 #read fibroblast dataset
-exp_matrix_filtered_fibro=read_excel("C:/Users/I0471594/OneDrive - Sanofi (1)/Desktop/multicellular_map_model/datasets/GSE109449/GSE109449_RA_fibroblast_SC_matrix.xlsx")
+exp_matrix_filtered_fibro=read_excel("Large-scale multicellular modeling of the arthritic joint\\Datasets\\GSE109449\\GSE109449_RA_fibroblast_SC_matrix.xlsx")
 
 colnames(exp_matrix_filtered_fibro)[1]="ID"
 exp_matrix_filtered_fibro=exp_matrix_filtered_fibro %>% select(matches("ID|RA"))
