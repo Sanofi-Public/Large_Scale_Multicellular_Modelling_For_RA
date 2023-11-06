a250fff449d3fc3d781c9a3fab2e2c3c8d6792ed
naouel zerrouk <nawelzerrouk@gmail.com>
2023-11-06 13:04:04 +0000

    Update Attractors_filtering.py

--- Models calibration/Attractors_search/Attractors_filtering.py
+++ Models calibration/Attractors_search/Attractors_filtering.py
@@ -6,7 +6,7 @@ it filters out the csv files where not all the nodes are stable
 import os
 import pandas as pd
 import glob
-my_path = os.path.join("a-computational-framework-to-build-and-calibrate-large-scale-boolean-models-main","Attractors_search","BioCheckConsoleMulti","BioCheckConsoleMulti","bin","Debug","netcoreapp3.1")
+my_path = os.path.join("Large-scale multicellular modeling of the arthritic joint","Attractors_search","BioCheckConsoleMulti","BioCheckConsoleMulti","bin","Debug","netcoreapp3.1")
 os.chdir(my_path)
 co=0
 maximum=glob.glob('*.csv')
@@ -15,6 +15,6 @@ while co < maximum :
     if file_exists:
         df = pd.read_csv("stable_nodes_"+str(co)+".csv", index_col=None, header=0)
         #check if all the nodes are stable
-        if len(df)!= 309:
+        if len(df)!= 275:
             os.remove("stable_nodes_"+str(co)+".csv")
     co=co+1
